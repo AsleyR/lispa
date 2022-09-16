f732f574311e231bb6d6d2aeecfbb4ed94aa9ea9
AsleyR <79556058+AsleyR@users.noreply.github.com>
2022-09-15 22:58:51 -0600

    Create initial react app

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="">
+      <h1 className='text-7xl font-public-sans font-bold'>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
